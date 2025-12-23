4513c95536a0809ca546111da78eb4c94e24ff5b
bwogt <bwogt.dev@gmail.com>
2025-12-23 08:38:42 -0300

    fix(test):  adds verification to new deposit response data

--- tests/Feature/Transactions/Deposit/DepositAccessTest.php
+++ tests/Feature/Transactions/Deposit/DepositAccessTest.php
@@ -4,12 +4,14 @@ namespace Tests\Feature\Transactions\Deposit;
 
 use App\Constants\Deposit\DepositConstants;
 use App\Enum\FlashMessage\FlashMessageType;
+use App\Enum\Transaction\TransactionStatus;
+use App\Enum\Transaction\TransactionType;
 use Illuminate\Testing\Fluent\AssertableJson;
 use Laravel\Sanctum\Sanctum;
 
 class DepositAccessTest extends DepositTestSetUp
 {
-    public function test_an_unauthenticated_user_should_not_be_able_to_deposit(): void
+    public function test_an_unauthenticated_user_should_not_be_able_to_deposit_funds(): void
     {
         $this->postJson($this->route(), ['value' => DepositConstants::MIN_VALUE])
             ->assertUnauthorized()
@@ -18,7 +20,7 @@ class DepositAccessTest extends DepositTestSetUp
             );
     }
 
-    public function test_a_consumer_user_should_be_able_to_deposit(): void
+    public function test_a_consumer_user_should_be_able_to_deposit_funds(): void
     {
         Sanctum::actingAs($this->consumer);
 
@@ -26,10 +28,14 @@ class DepositAccessTest extends DepositTestSetUp
             ->assertCreated()
             ->assertJson(fn (AssertableJson $json) => $json->where('message.type', FlashMessageType::SUCCESS)
                 ->where('message.text', trans('flash_messages.success.deposit'))
+                ->where('data.type', TransactionType::DEPOSIT->value)
+                ->where('data.status', TransactionStatus::COMPLETED->value)
+                ->has('data.created_at')
+                ->has('data.updated_at')
             );
     }
 
-    public function test_a_merchant_user_should_be_able_to_deposit(): void
+    public function test_a_merchant_user_should_be_able_to_deposit_funds(): void
     {
         Sanctum::actingAs($this->merchant);
 
@@ -37,6 +43,10 @@ class DepositAccessTest extends DepositTestSetUp
             ->assertCreated()
             ->assertJson(fn (AssertableJson $json) => $json->where('message.type', FlashMessageType::SUCCESS)
                 ->where('message.text', trans('flash_messages.success.deposit'))
+                ->where('data.type', TransactionType::DEPOSIT->value)
+                ->where('data.status', TransactionStatus::COMPLETED->value)
+                ->has('data.created_at')
+                ->has('data.updated_at')
             );
     }
 }
